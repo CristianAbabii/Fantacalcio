b1b67cf35ac8de8ee8bf515b48e34bb578695850
CristianAbabii <cristian.ababii@yahoo.com>
2018-06-20 14:58:05 +0200

    First commit

---
+++ src/main/java/it/dstech/computers/FantacalcioApplication.java
@@ -0,0 +1,12 @@
+package it.dstech.computers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantacalcioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantacalcioApplication.class, args);
+	}
+}
---
+++ src/test/java/it/dstech/computers/FantacalcioApplicationTests.java
@@ -0,0 +1,16 @@
+package it.dstech.computers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FantacalcioApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
