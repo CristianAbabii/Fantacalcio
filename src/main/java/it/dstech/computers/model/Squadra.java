87e635f642a282671ebde7c355978e07d1e9712f
Veshsare <lowe.blues.lb@gmail.com>
2018-06-20 16:52:17 +0200

    aggiunta entity Squadra

--- src/main/java/it/dstech/computers/model/Squadra.java
+++ src/main/java/it/dstech/computers/model/Squadra.java
@@ -1,5 +1,94 @@
 package it.dstech.computers.model;
 
-public class Squadra {
+import java.util.List;
+
+import javax.persistence.Column;
+
+public class Squadra extends Base {
+	
+	@Column(name = "nome", nullable = false, unique = true)
+	private String nome;
+	
+	//OneToOne
+	private Utente utente;
+	
+	//ManyToOne
+	private Campionato campionato;
+	
+	//OneToMany
+	private List<Giocatore> rosa;
+	
+	@Column(name = "punti")
+	private Integer punti;
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	/**
+	 * @return the utente
+	 */
+	public Utente getUtente() {
+		return utente;
+	}
+
+	/**
+	 * @param utente the utente to set
+	 */
+	public void setUtente(Utente utente) {
+		this.utente = utente;
+	}
+
+	/**
+	 * @return the campionato
+	 */
+	public Campionato getCampionato() {
+		return campionato;
+	}
+
+	/**
+	 * @param campionato the campionato to set
+	 */
+	public void setCampionato(Campionato campionato) {
+		this.campionato = campionato;
+	}
+
+	/**
+	 * @return the rosa
+	 */
+	public List<Giocatore> getRosa() {
+		return rosa;
+	}
+
+	/**
+	 * @param rosa the rosa to set
+	 */
+	public void setRosa(List<Giocatore> rosa) {
+		this.rosa = rosa;
+	}
+
+	/**
+	 * @return the punti
+	 */
+	public Integer getPunti() {
+		return punti;
+	}
+
+	/**
+	 * @param punti the punti to set
+	 */
+	public void setPunti(Integer punti) {
+		this.punti = punti;
+	}
 
 }
