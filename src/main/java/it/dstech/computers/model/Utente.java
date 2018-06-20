0e55f3feee0eb32676c91de5fd517581e8733688
vico <vicobb22@hotmail.com>
2018-06-20 16:05:21 +0200

    yes

--- src/main/java/it/dstech/computers/model/Utente.java
+++ src/main/java/it/dstech/computers/model/Utente.java
@@ -1,5 +1,21 @@
 package it.dstech.computers.model;
 
-public class Utente {
+import javax.persistence.Column;
+import javax.persistence.Entity;
 
-}
+import it.dstech.computers.model.UtenteProfile;
+
+@Entity (name = "utente")
+	public class Utente extends Base {
+
+		@Column (name = "username", nullable = false, unique = true)
+		private String username;
+		@Column (name = "password", nullable = false, unique = true)
+		private String password;
+		
+		@Column (name = "budget", nullable = false, unique = false)
+		private Integer budget;
+		
+		private UtenteProfile profileType;
+		
+	}
---
+++ src/main/java/it/dstech/computers/model/UtenteProfile.java
@@ -0,0 +1,5 @@
+package it.dstech.computers.model;
+
+public enum UtenteProfile {
+	ROLE_ADMIN, ROLE_USER, ROLE_DBA
+}
