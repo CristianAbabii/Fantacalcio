c6087324657fe6ddc2f3a58665bee5d381af3197
Pc <Pc@DESKTOP-9J6992O>
2018-06-21 10:55:42 +0200

    aggiunta: GiocatoreCtrl, GiocatoreService, package security e WebSecurityConfig.

---
+++ src/main/java/it/dstech/computers/api/GiocatoreCtrl.java
@@ -0,0 +1,47 @@
+package it.dstech.computers.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.dstech.computers.model.Giocatore;
+import it.dstech.computers.service.auth.GiocatoreService;
+
+
+
+@RestController
+@RequestMapping("/giocatore")
+public class GiocatoreCtrl {
+
+
+	@Autowired
+	private GiocatoreService serviceGiocatore;
+
+	@RequestMapping (method = RequestMethod.POST, value = "/create")
+	public Giocatore create (@RequestBody Giocatore giocatore) {
+		return serviceGiocatore.create(giocatore);
+	}
+	@RequestMapping (method = RequestMethod.GET, value = "/findOne/{id}")
+	public Giocatore findOne (@PathVariable Long id) throws Exception {
+		return serviceGiocatore.findOne(id);
+	}
+	@RequestMapping (method = RequestMethod.GET, value = "/findAll")
+	public Iterable<Giocatore> findAll() {
+		return serviceGiocatore.findAll();
+	}
+	@RequestMapping (method = RequestMethod.DELETE, value = "/deleteOne")
+	public void deleteOne (Long id) {
+		serviceGiocatore.deleteOne(id);
+	}
+	@RequestMapping (method = RequestMethod.DELETE, value = "/deleteAll") 
+	public void deleteAll() {
+		serviceGiocatore.deleteAll();
+	}
+	@RequestMapping (method = RequestMethod.PUT, value = "/update")
+	public Giocatore update (@RequestBody Giocatore giocatoreInput) throws Exception {
+		return serviceGiocatore.update(giocatoreInput);
+	}
+}
---
+++ src/main/java/it/dstech/computers/service/auth/GiocatoreService.java
@@ -0,0 +1,39 @@
+package it.dstech.computers.service.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.dstech.computers.model.Giocatore;
+import it.dstech.computers.repository.IGiocatoreRepository;
+
+@Service
+public class GiocatoreService {
+	
+	@Autowired
+	private IGiocatoreRepository dao;
+	
+	public Giocatore create (Giocatore giocatore) {
+		return dao.save(giocatore);
+	}
+	public Giocatore findOne (Long id) throws Exception {
+		return dao.findById(id).orElseThrow(()-> new Exception());
+	}
+	public Iterable<Giocatore> findAll() {
+		return dao.findAll();
+	}
+	public void deleteOne(Long id) {
+		dao.deleteById(id);
+	}
+	public void deleteAll() {
+		dao.deleteAll();
+	}
+	public Giocatore update (Giocatore giocatoreInput) throws Exception {
+		Giocatore giocatoreDb = findOne (giocatoreInput.getId());
+		giocatoreDb.setNome(giocatoreInput.getNome());
+		giocatoreDb.setCognome(giocatoreInput.getCognome());
+		giocatoreDb.setSquadraReale(giocatoreInput.getSquadraReale());
+		giocatoreDb.setRuolo(giocatoreInput.getRuolo());
+		giocatoreDb.setPrezzo(giocatoreInput.getPrezzo());
+		return dao.save(giocatoreDb);
+	}
+}
