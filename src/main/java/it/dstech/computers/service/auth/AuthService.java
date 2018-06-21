aa617911bc90f32e56b140a6f6522f91a63e1b10
antonellatroisi <antonella873@hotmail.it>
2018-06-21 11:06:53 +0200

    creazione authservice e customerUserDetailsService

--- src/main/java/it/dstech/computers/service/auth/AuthService.java
+++ src/main/java/it/dstech/computers/service/auth/AuthService.java
@@ -1,5 +1,44 @@
 package it.dstech.computers.service.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import it.dstech.computers.model.Utente;
+
+
+
+@Service
 public class AuthService {
 
+	@Autowired
+	private AuthenticationManager autenthicationManager;
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	/**
+	 * Authentication e user
+	 * 
+	 * @param user
+	 * @return
+	 * @throws Exception
+	 */
+	public UserDetails authenticate (Utente utente) throws Exception {
+		UserDetails loadUserByUsername = userDetailsService.loadUserByUsername(utente.getUsername());
+
+		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loadUserByUsername.getUsername(), utente.getPassword());
+
+
+		Authentication authentication = autenthicationManager.authenticate(authenticationToken);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+
+		return loadUserByUsername;
+	}
 }
+
--- src/main/java/it/dstech/computers/service/auth/CustomerUserDetailsService.java
+++ src/main/java/it/dstech/computers/service/auth/CustomerUserDetailsService.java
@@ -1,5 +1,46 @@
 package it.dstech.computers.service.auth;
 
-public class CustomerUserDetailsService {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import it.dstech.computers.model.Utente;
+import it.dstech.computers.service.UtenteService;
+
+@Service
+public class CustomerUserDetailsService implements UserDetailsService {
+
+	private static final Logger logger = java.util.logging.Logger.getLogger(CustomerUserDetailsService.class.getName());
+
+	@Autowired
+	private UtenteService userService;
+
+	@Override
+	@Transactional(readOnly = true)
+	public UserDetails loadUserByUsername (String username) throws UsernameNotFoundException {
+		Utente utente = userService.findByUsername(username);
+		logger.info("User: " + username);
+		if (utente == null) {
+			logger.info ("User not found");
+			throw new UsernameNotFoundException ("Username not found");
+		}
+		return new org.springframework.security.core.userdetails.User(utente.getUsername(), utente.getPassword(), true, true, true, true, getGrantedAuthorities(utente));
+	}
+	private List<GrantedAuthority> getGrantedAuthorities(Utente utente) {
+		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+
+		authorities.add(new SimpleGrantedAuthority ("" + utente.getProfileType()));
+
+		logger.info("Authorities: " + authorities);
+		return authorities;
+	}
 }
