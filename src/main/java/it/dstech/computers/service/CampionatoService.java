8efa2f705f9df7e9ea5e2548169e7ab6b62b6e40
Veshsare <lowe.blues.lb@gmail.com>
2018-06-22 12:49:50 +0200

    added method classifica

--- src/main/java/it/dstech/computers/service/CampionatoService.java
+++ src/main/java/it/dstech/computers/service/CampionatoService.java
@@ -1,9 +1,13 @@
 package it.dstech.computers.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import it.dstech.computers.model.Campionato;
+import it.dstech.computers.model.Squadra;
 import it.dstech.computers.repository.ICampionatoRepository;
 
 @Service
@@ -48,6 +52,28 @@ public class CampionatoService {
 		old.setUtenti(c.getUtenti());
 		
 		return old;
+	}
+	
+	public Iterable<Squadra> classifica(Campionato c) {
+		
+		List<Squadra> listaSquadre = c.getSquadre();
+				
+		Squadra temp = new Squadra();
+		
+		for(int i = 0; i < listaSquadre.size(); i ++) {
+			for(int j = 0; j < listaSquadre.size(); j++) {
+				if(listaSquadre.get(i).getPunti() < listaSquadre.get(j).getPunti()) {
+					Squadra due = listaSquadre.get(j);
+					temp = listaSquadre.get(i);
+					listaSquadre.add(i, due);
+					listaSquadre.add(j, temp);
+				}
+			}
+		}
+		
+		
+		return listaSquadre;
+
 	}
 
 }
