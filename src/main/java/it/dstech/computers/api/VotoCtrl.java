ef06bc6931356f5b650da019b8822e3c57d37e79
antonellatroisi <antonella873@hotmail.it>
2018-06-22 15:13:55 +0200

    ctrl voto

--- src/main/java/it/dstech/computers/api/VotoCtrl.java
+++ src/main/java/it/dstech/computers/api/VotoCtrl.java
@@ -1,11 +1,24 @@
 package it.dstech.computers.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import it.dstech.computers.model.Voto;
+import it.dstech.computers.service.VotoService;
 
 @RestController
 @RequestMapping("/voto")
 public class VotoCtrl {
 
+	@Autowired
+	private VotoService service;
+	
+	@RequestMapping (method = RequestMethod.POST, value = "/create")
+	public Voto create (@RequestBody Voto v) {
+		return service.create(v);
+	}
 	
 }
