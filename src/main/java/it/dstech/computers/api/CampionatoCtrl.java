481afdad3dc90295658a0cc337a4a6e7d5ad90c0
antonellatroisi <antonella873@hotmail.it>
2018-06-22 14:39:24 +0200

    creazione ctrl campionato, squadra, voto

---
+++ src/main/java/it/dstech/computers/api/CampionatoCtrl.java
@@ -0,0 +1,10 @@
+package it.dstech.computers.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/campionato")
+public class CampionatoCtrl {
+
+}
---
+++ src/main/java/it/dstech/computers/api/VotoCtrl.java
@@ -0,0 +1,11 @@
+package it.dstech.computers.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/voto")
+public class VotoCtrl {
+
+	
+}
